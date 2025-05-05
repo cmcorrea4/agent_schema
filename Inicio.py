b5610f2735c351321e76f7478e261452105cc65a
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2025-05-05 14:32:39 -0500

    Update Inicio.py

--- Inicio.py
+++ Inicio.py
@@ -170,7 +170,7 @@ st.markdown("""
             <span style="font-weight: 500; color: #BBDEFB;">¿Puedes darme el consumo de Energía de las últimas 2 horas y su coste correspondiente?</span>
         </li>
         <li style="margin-bottom: 0.8rem; padding: 0.5rem 0.8rem; background-color: rgba(30, 136, 229, 0.1); border-radius: 4px; border-left: 3px solid #FF9800;">
-            <span style="font-weight: 500; color: #BBDEFB;">¿Puedes darme el gráfico de demanda de energía de las últimas 3 horas?</span>
+            <span style="font-weight: 500; color: #BBDEFB;">¿Puedes darme el gráfico de demanda de energía de la última hora?</span>
         </li>
         <li style="margin-bottom: 0.8rem; padding: 0.5rem 0.8rem; background-color: rgba(30, 136, 229, 0.1); border-radius: 4px; border-left: 3px solid #FF9800;">
             <span style="font-weight: 500; color: #BBDEFB;">¿Qué patrones de demanda de energía se han dado en la última hora?</span>
